1aefe1a646780dfe097e1b2fb6d621d4757440a2
Saffi Khawaja <saffikhawaja@gmail.com>
2026-05-27 23:47:07 +0500

    chore: update assembly informational version and cache files across multiple projects

--- src/SahaaraAI.Infrastructure/obj/Debug/net10.0/SahaaraAI.Infrastructure.AssemblyInfo.cs
+++ src/SahaaraAI.Infrastructure/obj/Debug/net10.0/SahaaraAI.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SahaaraAI.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e075b3142e8dcf0f447b27d2ef0e0948e9c5a1e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f498477d39fc046ea941fe173060e39dec4a9cd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("SahaaraAI.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SahaaraAI.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- tests/SahaaraAI.ArchitectureTests/obj/Debug/net10.0/SahaaraAI.ArchitectureTests.AssemblyInfo.cs
+++ tests/SahaaraAI.ArchitectureTests/obj/Debug/net10.0/SahaaraAI.ArchitectureTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SahaaraAI.ArchitectureTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a75359568e4f9361b50850442bb46898980af43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f498477d39fc046ea941fe173060e39dec4a9cd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("SahaaraAI.ArchitectureTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SahaaraAI.ArchitectureTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
